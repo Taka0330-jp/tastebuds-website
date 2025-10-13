f1197647b956542fff168a41c934757f19e30f73
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-13 12:43:38 -0700

    add best blog

--- scripts/bestPost.js
+++ scripts/bestPost.js
@@ -1,25 +1,21 @@
-import { posts } from "/scripts/data.js";
+import { posts } from "../scripts/data.js";
 
 
+const mount = document.querySelector('[data-mount="best"]');
 
-const mount = document.querySelector('[data-mount="recent"]');
-console.log(mount);
+const best = posts.filter(p => p.category === "Best");
+console.log(best);
 
-const recent = posts.filter(p => p.category === "Recent");
-
-console.log(recent);
-
-const [featured, ...rest] = recent;
 
 mount.innerHTML = `
-  <article class="featured">
-    <img src="${featured.image}" alt="${featured.alt}" loading="lazy">
-    <div class ="text-container">
-      <div class="head-body">
-        <h3 class="card__title">${featured.title}</h3>
-        <p class="card__excerpt">${featured.excerpt}</p>
-      </div>
-        <div class="link">
+<article class ="best-cards">
+<img src="${best[0].image}" alt="${best[0].alt}" loading="lazy">
+<div class = "text-container">
+<div class="head-body">
+<h3 class="card__title">${best[0].title}</h3>
+<p class="card__excerpt">${best[0].excerpt}</p>
+</div>
+<div class="link">
                     <div class="icons">
                       <a href="#"
                                  class="tiktok-link" >
@@ -42,46 +38,73 @@ mount.innerHTML = `
                     </div>
                               <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
                   </div>
-    </div>
-  </article>
+</div>
+</article>
+<article class ="best-cards">
+<img src="${best[1].image}" alt="${best[1].alt}" loading="lazy">
+<div class = "text-container">
+<div class="head-body">
+<h3 class="card__title">${best[1].title}</h3>
+<p class="card__excerpt">${best[1].excerpt}</p>
+</div>
+<div class="link">
+                    <div class="icons">
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/TikTok.svg"
+                                     alt="TikTok Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/YouTube.svg"
+                                     alt="YouTube Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/Instagram Circle.svg"
+                                     alt="Instagram Icon"
+                                     class="icon">
+                              </a>
+                    </div>
+                              <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
+                  </div>
+</div>
+</article>
+<article class ="best-cards">
+<img src="${best[2].image}" alt="${best[2].alt}" loading="lazy">
+<div class = "text-container">
+<div class="head-body">
+<h3 class="card__title">${best[2].title}</h3>
+<p class="card__excerpt">${best[2].excerpt}</p>
+</div>
+<div class="link">
+                    <div class="icons">
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/TikTok.svg"
+                                     alt="TikTok Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/YouTube.svg"
+                                     alt="YouTube Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/Instagram Circle.svg"
+                                     alt="Instagram Icon"
+                                     class="icon">
+                              </a>
+                    </div>
+                              <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
+                  </div>
+</div>
+</article>
 
-  ${rest
-    .slice(0, 3)
-    .map(
-      (p, i) => `
-      <article class="small small-${i + 1}">
-        <img src="${p.image}" alt="${p.alt}" loading="lazy">
-        <div class ="text-container">
-          <div class="head-body">
-            <h3 class="card__title">${p.title}</h3>
-            <p class="card__excerpt">${p.excerpt}</p>
-          </div>
-                  <div class="link">
-                    <div class="icons">
-                      <a href="#"
-                                 class="tiktok-link" >
-                                <img src="../Assets/icons/TikTok.svg"
-                                     alt="TikTok Icon"
-                                     class="icon">
-                              </a>
-                      <a href="#"
-                                 class="tiktok-link" >
-                                <img src="../Assets/icons/YouTube.svg"
-                                     alt="YouTube Icon"
-                                     class="icon">
-                              </a>
-                      <a href="#"
-                                 class="tiktok-link" >
-                                <img src="../Assets/icons/Instagram Circle.svg"
-                                     alt="Instagram Icon"
-                                     class="icon">
-                              </a>
-                    </div>
-                              <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
-                  </div>
-        </div>
-      </article>
-    `
-    )
-    .join("")}
-`;
+`
+
+
