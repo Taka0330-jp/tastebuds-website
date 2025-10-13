366a789daee841ed09c09b58863f1d91024b0ac0
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-12 20:55:24 -0700

    style recent post section
    change css to scss

--- scripts/recentPost.js
+++ scripts/recentPost.js
@@ -14,8 +14,35 @@ const [featured, ...rest] = recent;
 mount.innerHTML = `
   <article class="featured">
     <img src="${featured.image}" alt="${featured.alt}" loading="lazy">
-    <h3 class="card__title">${featured.title}</h3>
-    <p class="card__excerpt">${featured.excerpt}</p>
+    <div class ="text-container">
+      <div class="head-body">
+        <h3 class="card__title">${featured.title}</h3>
+        <p class="card__excerpt">${featured.excerpt}</p>
+      </div>
+        <div class="link">
+                    <div class="icons">
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/TikTok.svg"
+                                     alt="TikTok Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/YouTube.svg"
+                                     alt="YouTube Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/Instagram Circle.svg"
+                                     alt="Instagram Icon"
+                                     class="icon">
+                              </a>
+                    </div>
+                              <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
+                  </div>
+    </div>
   </article>
 
   ${rest
@@ -24,8 +51,35 @@ mount.innerHTML = `
       (p, i) => `
       <article class="small small-${i + 1}">
         <img src="${p.image}" alt="${p.alt}" loading="lazy">
-        <h3 class="card__title">${p.title}</h3>
-        <p class="card__excerpt">${p.excerpt}</p>
+        <div class ="text-container">
+          <div class="head-body">
+            <h3 class="card__title">${p.title}</h3>
+            <p class="card__excerpt">${p.excerpt}</p>
+          </div>
+                  <div class="link">
+                    <div class="icons">
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/TikTok.svg"
+                                     alt="TikTok Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/YouTube.svg"
+                                     alt="YouTube Icon"
+                                     class="icon">
+                              </a>
+                      <a href="#"
+                                 class="tiktok-link" >
+                                <img src="../Assets/icons/Instagram Circle.svg"
+                                     alt="Instagram Icon"
+                                     class="icon">
+                              </a>
+                    </div>
+                              <div class="read-btn"><a href="home.html"><span>Read</span></a></div>
+                  </div>
+        </div>
       </article>
     `
     )
