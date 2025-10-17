0d047384a5683a04184b3bc476ecd3162c2b04f6
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-17 11:56:46 -0700

    delete bestPost.js

--- scripts/HomePost.js
+++ scripts/HomePost.js
@@ -1,13 +1,19 @@
 import { posts } from "/scripts/data.js";
 
+//Get DOM Elements
 
 const recentGrid = document.querySelector('.recent-grid');
 const tplFeatured = document.getElementById('tplFeatured');
 const tplSmall = document.getElementById("tplSmall");
+const bestTpl = document.getElementById("tplBest");
+const bestGrid = document.querySelector(".best-grid");
 
+//Filtered by Category
 
 const recent = posts.filter(p => p.category === "Recent");
+const bestPosts = posts.filter(bp => bp.category === "Best");
 
+//Split the array of Recent items into featured for the first item and rest for the subsequent items.
 
 const [featured, ...rest] = recent;
 
@@ -24,10 +30,12 @@ function fillCard(fragment, p){
   return fragment;
 }
 
-{
+//Recent Featured
+
   const frag = tplFeatured.content.cloneNode(true);
   recentGrid.appendChild(fillCard(frag, featured));
-}
+
+//Recent Small
 
 rest.slice(0, 3).forEach((p, i) => {
   const frag = tplSmall.content.cloneNode(true);
@@ -37,4 +45,11 @@ rest.slice(0, 3).forEach((p, i) => {
   article.classList.add(`small-${i + 1}`);
 
   recentGrid.appendChild(filled);
+});
+
+//Best Posts
+bestPosts.forEach((bp) =>{
+  const frag = bestTpl.content.cloneNode(true);
+  const filled = fillCard(frag, bp);
+  bestGrid.appendChild(filled)
 });
