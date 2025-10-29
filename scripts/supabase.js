80ed1656e4c356b1c2c1c5741101017921554f5c
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-29 14:54:19 -0700

    add comment function

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -2,19 +2,19 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "/scripts/config.js";
 
 // client
- const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 // test
- async function testSupabase() {
-    const {data, error} = await supabase.from("comments").select("*").limit(1);
-    if(error) throw error;
-    return data;
-}
+//  async function testSupabase() {
+//     const {data, error} = await supabase.from("comments").select("*").limit(1);
+//     if(error) throw error;
+//     return data;
+// }
 
 // get comment
 
- async function listComments(postId, limit = 50) {
+ export async function listComments(postId, limit = 50) {
   const { data, error } = await supabase
     .from("comments")
     .select("id, author_name, body, created_at")
@@ -26,16 +26,16 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from "/scripts/config.js";
 }
 
 //add comment
- async function addComment({ postId, authorName, body }) {
+ export async function addComment({ postId, authorName, body }) {
   const { error } = await supabase
     .from("comments")
     .insert([{ post_id: postId, author_name: authorName ?? null, body }]);
   if (error) throw error;
 }
 
-  testSupabase()
-    .then(d => console.log("✅ Supabase OK:", d))
-    .catch(e => console.error("❌ Supabase NG:", e));
+  // testSupabase()
+  //   .then(d => console.log("✅ Supabase OK:", d))
+  //   .catch(e => console.error("❌ Supabase NG:", e));
 
-    const POST_ID = location.pathname || "/";
-  listComments(POST_ID).then(rows => console.log("📥 comments:", rows));
+  //   const POST_ID = location.pathname || "/";
+  // listComments(POST_ID).then(rows => console.log("📥 comments:", rows));
