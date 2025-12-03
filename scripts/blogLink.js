ab13962112b4782f9abd6fb25b2c53709138caf2
Taka0330-jp <gtakanari0330@gmail.com>
2025-12-02 22:59:12 -0800

    add sidebar

---
+++ scripts/blogLink.js
@@ -0,0 +1,26 @@
+import { posts } from "../scripts/data.js";
+
+const RECENT_CATEGORY = "Recent";
+const SIDEBAR_LIST_SELECTOR = ".sidebar__list";
+
+function renderRecentPosts() {
+  const list = document.querySelector(SIDEBAR_LIST_SELECTOR);
+  if (!list) return;
+
+  const recentPosts = posts.filter((post) => post.category === RECENT_CATEGORY);
+  const fragment = document.createDocumentFragment();
+
+  recentPosts.forEach((post) => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = post.url;
+    link.textContent = post.title;
+    li.append(link);
+    fragment.append(li);
+  });
+
+  list.innerHTML = "";
+  list.append(fragment);
+}
+
+document.addEventListener("DOMContentLoaded", renderRecentPosts);
